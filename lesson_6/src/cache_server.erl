745df8431276915de4263ea316f82050a3fcb4c4
drEnilight <vladyslav.romaniuk@outlook.com>
2020-12-16 15:33:22 +0200

    lesson_6: improve some moments after review

--- lesson_6/src/cache_server.erl
+++ lesson_6/src/cache_server.erl
@@ -3,11 +3,10 @@
 
 %% API.
 -export([start_link/2]).
--export([new/1]).
 -export([stop/0]).
--export([insert/4]).
--export([lookup/2]).
--export([lookup_by_date/3]).
+-export([insert/3]).
+-export([lookup/1]).
+-export([lookup_by_date/2]).
 
 %% Callbacks.
 -export([init/1]).
@@ -19,32 +18,30 @@
 % API
 
 start_link(TableName, [{drop_interval, Interval}]) ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [TableName, Interval], []).
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [TableName, Interval*1000], []).
 
 stop() ->
 	gen_server:call(?MODULE, stop).
 
-new(TableName) ->
-  gen_server:cast(?MODULE, {new, TableName}).
+insert(Key, Value, TTL) ->
+  gen_server:cast(?MODULE, {insert, Key, Value, TTL}).
 
-insert(TableName, Key, Value, TTL) ->
-  gen_server:cast(?MODULE, {insert, TableName, Key, Value, TTL}).
+lookup(Key) ->
+  gen_server:call(?MODULE, {lookup, Key}).
 
-lookup(TableName, Key) ->
-  gen_server:call(?MODULE, {lookup, TableName, Key}).
-
-lookup_by_date(TableName, DateFrom, DateTo) ->
-  gen_server:call(?MODULE, {lookup_by_date, TableName, DateFrom, DateTo}).
+lookup_by_date(DateFrom, DateTo) ->
+  gen_server:call(?MODULE, {lookup_by_date, DateFrom, DateTo}).
 
 % Callbacks
 
 init([TableName, Interval]) ->
-  erlang:send_after(Interval, self(), {delete_obsolete, {TableName, Interval}}),
-  {ok, self()}.
+  cache_helper:create(TableName),
+  erlang:send_after(Interval, self(), delete_obsolete),
+  {ok, {TableName, Interval}}.
 
-handle_info({delete_obsolete, {TableName, Interval}}, State) ->
+handle_info(delete_obsolete, {TableName, Interval} = State) ->
   cache_helper:delete_obsolete(TableName),
-  erlang:send_after(Interval, self(), {delete_obsolete, {TableName, Interval}}),
+  erlang:send_after(Interval, self(), delete_obsolete),
   {noreply, State};
 handle_info(_Info, State) ->
 	{noreply, State}.
@@ -52,17 +49,14 @@ handle_info(_Info, State) ->
 terminate(_Reason, _State) ->
 	ok.
 
-handle_cast({new, TableName}, State) ->
-  cache_helper:create(TableName),
-  {noreply, State};
-handle_cast({insert, TableName, Key, Value, TTL}, State) ->
+handle_cast({insert, Key, Value, TTL}, {TableName, _} = State) ->
   cache_helper:insert(TableName, Key, Value, TTL),
   {noreply, State}.
 
-handle_call({lookup, TableName, Key}, _From, State) ->
+handle_call({lookup, Key}, _From, {TableName, _} = State) ->
   Result = cache_helper:lookup(TableName, Key),
   {reply, Result, State};
-handle_call({lookup_by_date, TableName, DateFrom, DateTo}, _From, State) ->
+handle_call({lookup_by_date, DateFrom, DateTo}, _From, {TableName, _} = State) ->
   Values = cache_helper:lookup(TableName, DateFrom, DateTo),
   {reply, {ok, Values}, State};
 handle_call(stop, _From, State) ->
--- lesson_6/test/cache_server_SUITE.erl
+++ lesson_6/test/cache_server_SUITE.erl
@@ -6,7 +6,6 @@
 
 all() ->
   [
-    new_table,
     insert_into,
     lookup_by_key,
     lookup_by_date
@@ -21,23 +20,18 @@ end_per_suite(_Config) ->
 init_per_testcase(Case, Config) ->
   TableName = ?config(table_name, Config),
   cache_server:start_link(TableName, [{drop_interval, 600000}]),
-  cache_server:new(cache_server),
   Config.
-
-new_table(Config) ->
-  ok = cache_server:new(new_table).
 
 insert_into(Config) ->
   TableName = ?config(table_name, Config),
-  cache_server:new(new_table),
-  ok = cache_server:insert(TableName, key, value, 600).
+  ok = cache_server:insert(key, value, 600).
 
 lookup_by_key(Config) ->
   TableName = ?config(table_name, Config),
-  cache_server:insert(TableName, age, 40, 600),
-  {ok, 40} = cache_server:lookup(TableName, age).
+  cache_server:insert(age, 40, 600),
+  {ok, 40} = cache_server:lookup(age).
 
 lookup_by_date(Config) ->
   TableName = ?config(table_name, Config),
-  cache_server:insert(TableName, age, 40, 600),
-  {ok, [40]} = cache_server:lookup_by_date(TableName, {{2015,1,1},{00,00,00}}, {{2020,12,31},{00,00,00}}).
+  cache_server:insert(age, 40, 600),
+  {ok, [40]} = cache_server:lookup_by_date({{2015,1,1},{00,00,00}}, {{2020,12,31},{00,00,00}}).
